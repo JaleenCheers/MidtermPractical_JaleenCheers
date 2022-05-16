d23898157f1aebadfc980cdbd3a9f5dbe9545781
Jaleen Cheers <JTCheers@student.fullsail.edu>
2022-05-16 13:36:58 -0400

    Revert "Changed math problem return type"
    This reverts commit 16b0be14c955d7b1582bfc749a35f4dbb4d3cbf7.

--- MidtermPractical_JaleenCheers/MidtermPractical_JaleenCheers.cpp
+++ MidtermPractical_JaleenCheers/MidtermPractical_JaleenCheers.cpp
@@ -3,22 +3,17 @@
 
 #include <iostream>
 
-int MathProblem(int _rand1, int _rand2, int _op);
-void Problem();
-bool ValidateInt();
-
 int main()
 {
 	srand(time(0));
 	int rand1 = rand();
 	int rand2 = rand();
 	int op = rand() % 5;
-	MathProblem(rand1, rand2, op);
 
 
 }
 
-int MathProblem(int _rand1, int _rand2, int _op) {
+void MathProblem(int _rand1, int _rand2, int _op) {
 	char opChar;
 	int userInput;
 	bool loop = false;
